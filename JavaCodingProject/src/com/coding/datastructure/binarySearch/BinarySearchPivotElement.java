8a54189c49865045249828c5d6e07d946d7d3ebd
Mayankyadav123 <mayank1yadav1@gmail.com>
2019-03-22 11:18:07 +0530

    Pivot Element search

--- JavaCodingProject/src/com/coding/datastructure/binarySearch/BinarySearchPivotElement.java
+++ JavaCodingProject/src/com/coding/datastructure/binarySearch/BinarySearchPivotElement.java
@@ -5,11 +5,25 @@ public class BinarySearchPivotElement {
 	public static int binarySearchPivotElement(int arr[]) {
 		int l=0;
 		int h=arr.length-1;
-		return 0;  
+		int mid=0;
+		while(l<=h) {
+			mid=(l+h)/2;
+			if(mid<h&&arr[mid]>arr[mid+1]) {
+				return mid;
+			}else if(mid>l && arr[mid]<arr[mid-1]) {
+				return mid-1;
+			}
+			if(arr[l]<=arr[mid]) {
+				l=mid+1;
+			}else {
+				h=mid-1;
+			}
+		}
+		return -1;  
 	} 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {7,8,1,2,3,4,5,6};
+		int arr[]= {2,3,4,5,16,34,1};
 		int result=binarySearchPivotElement(arr);
 		System.out.println(result);
 	}
