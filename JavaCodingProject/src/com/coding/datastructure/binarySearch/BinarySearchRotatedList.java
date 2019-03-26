c7f91a3a0cc62a60273af98f143fa565d2adc7a3
myadav <mayank1yadav1@gmail.com>
2019-03-26 19:17:34 +0530

    rotated list binary search

--- JavaCodingProject/src/com/coding/datastructure/binarySearch/BinarySearchRotatedList.java
+++ JavaCodingProject/src/com/coding/datastructure/binarySearch/BinarySearchRotatedList.java
@@ -3,8 +3,37 @@ package com.coding.datastructure.binarySearch;
 public class BinarySearchRotatedList {
 	
 	public static int bsSearchRotatedList(int[] arr,int key) {
-		int index=pivotElement(arr);
+		int index=0;
+		int pivot=pivotElement(arr);
+		
+		if(pivot== -1) {
+			index=bs(arr,0,arr.length-1,key);
+		}
+		if(arr[pivot]==key) {
+			return pivot;
+		}
+		if(key<arr[0]) {
+			index=bs(arr,pivot+1,arr.length-1,key);
+		}else {
+			index=bs(arr,0,pivot-1,key);
+		}
 		return index;
+	}
+	
+	public static int bs(int[] arr,int l,int h,int key) {
+		while(l<=h) {
+			int mid=(l+h)/2;
+			if(arr[mid]==key) {
+				return mid;
+			}
+			if(key>arr[mid]) {
+				l=mid+1;
+			}
+			if(key<arr[mid]) {
+				h=mid-1;
+			}
+		}
+		return -1;
 	}
 
 	public static int pivotElement(int[] arr) {
@@ -29,7 +58,7 @@ public class BinarySearchRotatedList {
 	
 	public static void main(String[] args) {
 		int arr[]= {7,8,9,1,2,3,4,5,6};
-		int key=3;
+		int key=6;
 		int result=bsSearchRotatedList(arr,key);
 		System.out.println(result);
 	}
---
+++ JavaCodingProject/src/com/coding/interview/string/HashCode.java
@@ -0,0 +1,11 @@
+package com.coding.interview.string;
+
+public class HashCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String str="hello";
+			str.hashCode();
+	}
+
+}
