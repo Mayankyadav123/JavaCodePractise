b2afc697b57b12cba8176807cc9539a747e2864f
myadav <mayank1yadav1@gmail.com>
2019-03-28 15:29:21 +0530

    deadlock code added

--- JavaCodingProject/src/com/coding/threading/Deadlock.java
+++ JavaCodingProject/src/com/coding/threading/Deadlock.java
@@ -4,17 +4,54 @@ public class Deadlock {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SharedResource resources= new SharedResource("resource1", "resource2");
+		final Object resource1= new Object();
+		final Object resource2= new Object();
+		Thread t1= new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				synchronized (resource1) {
+					System.out.println("Holdin lock 1");
+					System.out.println("Thread1: locked resource1");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					synchronized (resource2) {
+					System.out.println("Thread1: locked resource2 after resource1");	
+					}
+				}
+			}
+		});
+		
+		Thread t2= new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				synchronized (resource2) {
+					System.out.println("Holdin lock 2");
+					System.out.println("Thread2: locked resource 2");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					synchronized(resource1) {
+						System.out.println("Thread2: locked resource 1 after resource 2");
+					}
+				}
+			}
+		});
+		t1.start();
+		t2.start();
 		
 	}
 }
 
-class SharedResource{
-	String resource1=null;
-	String resource2=null;
-	public SharedResource(String resource1,String resource2) {
-		super();
-		this.resource1=resource1;
-		this.resource2=resource2;
-	}
-}
+
+
