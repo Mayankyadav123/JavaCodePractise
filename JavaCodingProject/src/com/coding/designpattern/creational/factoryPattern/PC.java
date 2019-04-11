38c15b1e2a22b75de566b0afabea8e7d746cd292
myadav <mayank1yadav1@gmail.com>
2019-04-11 22:31:56 +0530

    Factory Pattern

---
+++ JavaCodingProject/src/com/coding/designpattern/creational/factoryPattern/PC.java
@@ -0,0 +1,65 @@
+package com.coding.designpattern.creational.factoryPattern;
+
+public class PC extends Computer{
+
+	private String ram;
+	private String hdd;
+	private String cpu;
+	
+	
+	
+	public PC(String ram, String hdd, String cpu) {
+		super();
+		this.ram = ram;
+		this.hdd = hdd;
+		this.cpu = cpu;
+	}
+
+	@Override
+	public String toString() {
+		return "PC [ram=" + ram + ", hdd=" + hdd + ", cpu=" + cpu + "]";
+	}
+
+	public String getRam() {
+		return ram;
+	}
+
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+
+	public String getHdd() {
+		return hdd;
+	}
+
+	public void setHdd(String hdd) {
+		this.hdd = hdd;
+	}
+
+	public String getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+
+	@Override
+	public String getRAM() {
+		// TODO Auto-generated method stub
+		return this.ram;
+	}
+
+	@Override
+	public String getHDD() {
+		// TODO Auto-generated method stub
+		return this.hdd;
+	}
+
+	@Override
+	public String getCPU() {
+		// TODO Auto-generated method stub
+		return this.cpu;
+	}
+
+}
