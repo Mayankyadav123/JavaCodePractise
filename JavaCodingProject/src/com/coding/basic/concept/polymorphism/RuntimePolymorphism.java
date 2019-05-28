fd32b9ff2f3e96e81a85b4a5367426e655539193
myadav <mayank1yadav1@gmail.com>
2019-05-28 17:44:39 +0530

    # WARNING: head commit changed in the meantime
    completed

--- JavaCodingProject/src/com/coding/basic/concept/polymorphism/RuntimePolymorphism.java
+++ JavaCodingProject/src/com/coding/basic/concept/polymorphism/RuntimePolymorphism.java
@@ -4,8 +4,12 @@ public class RuntimePolymorphism {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//HERE we use parent type to hold the object
+		//If the method is ovverriden by child it will call
+		//child method
 		Animal horse=new Horse();
 		Animal cat=new Cat();
+		
 		horse.sound();
 		cat.sound();
 	}
